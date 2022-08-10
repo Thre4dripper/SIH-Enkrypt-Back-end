13e2707355083d79010ef55b1f25b3a6357a12c8
Ijlal Ahmad <ijlalahmad845@gmail.com>
2022-08-10 15:59:04 +0530

    created login validation along with increasing pattern for wrong attempts

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -4,6 +4,37 @@ const path = require("path");
 const randomBinary = require("../utils/randomBinary");
 const generatePattern = require("../utils/generatePattern");
 
+/** =========================== FUNCTION FOR CREATING AND STORING LOGIN PATTERN  ==============================*/
+const createLoginPattern = (init, usersArray, username) => {
+  const user = usersArray.find((user) => user.username === username);
+  let pattern = user.pattern;
+
+  pattern = pattern.split(":")[0];
+
+  //randomBinary function is used to generate pattern
+
+  /*
+  IT WILL RETURN PATTERN WITH LENGTH OF 5 WHEN PATTERN IS EMPTY
+  IT WILL RETURN PATTERN WITH LENGTH INCREASED BY 1 WHEN PATTERN IS NOT EMPTY
+  */
+  if (init) {
+    pattern = randomBinary("");
+  } else {
+    pattern = randomBinary(pattern);
+  }
+
+  //writing generated pattern to user's data along with timestamp
+  pattern += ":" + new Date().getTime();
+  user.pattern = pattern;
+
+  //writing data into DB
+  fsPromises.writeFile(
+    path.join(__dirname, "..", "models", "users.json"),
+    JSON.stringify(usersArray)
+  );
+};
+
+/** =========================== FUNCTION FOR SENDING RANDOM IMAGE PATTERN TO CLIENT ==============================*/
 const imagePattern = async (req, res) => {
   const { username } = req.body;
 
@@ -20,35 +51,78 @@ const imagePattern = async (req, res) => {
     )
   );
   const user = usersArray.find((user) => user.username === username);
-  const { category, pass_image } = user;
-  const time = new Date().getTime() + 5 * 60 * 1000;
+  let { pattern, category, pass_image } = user;
 
-  //getting pattern value
-  const pattern = randomBinary("");
+  //getting pass_image value and login pattern value
+  pass_image = pass_image.substring(pass_image.length - 1);
+  pattern = pattern.split(":")[0];
+
+  //creating login pattern for storing in db
+  createLoginPattern(true, usersArray, username);
 
   //generating image pattern to be sent to client
-  const patternArray = generatePattern(pattern, 5, pass_image);
-
-  //writing generated pattern to user's data
-  pattern += ":" + time;
-  user.pattern = pattern;
-
-  //writing data into DB
-  await fsPromises.writeFile(
-    path.join(__dirname, "..", "models", "users.json"),
-    JSON.stringify(usersArray)
-  );
+  const imagesPattern = generatePattern(pattern, 5, pass_image);
 
   return res.status(200).json({
     message: "pattern generated",
     isExist: true,
-    pattern: patternArray,
+    pattern: imagesPattern,
     category,
   });
 };
 
+/** =========================== FUNCTION FOR FINAL VALIDATION OF USER BY PATTERN  ==============================*/
 const validateLogin = async (req, res) => {
-  const { username, pattern } = req.body;
+  const { username, pattern, timestamp } = req.body;
+
+  if (!username || !pattern || !timestamp) {
+    return res.status(400).json({ message: "Empty Field(s)", success: false });
+  }
+
+  try {
+    const usersArray = JSON.parse(
+      await fsPromises.readFile(
+        path.join(__dirname, "..", "models", "users.json"),
+        "utf8"
+      )
+    );
+
+    const user = usersArray.find((user) => user.username === username);
+    const dbPattern = user.pattern.split(":")[0];
+    const dbTimestamp = +user.pattern.split(":")[1];
+
+    //checking validity of pattern by timestamp
+    if (timestamp > dbTimestamp) {
+      return res.status(401).json({
+        message: "Pattern expired",
+        success: false,
+      });
+    }
+
+    //comparing pattern
+    if (dbPattern === pattern) {
+      //returning success message
+      return res.status(200).json({
+        message: "pattern validated",
+        success: true,
+      });
+    } else {
+      //for every wrong attemp, pattern is reset and increased by 1
+      createLoginPattern(false, usersArray, username);
+
+      //returning failure message
+      return res.status(401).json({
+        message: "pattern not validated",
+        success: false,
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      message: "Something went wrong",
+      success: false,
+    });
+  }
 };
 
 module.exports = { imagePattern, validateLogin };
