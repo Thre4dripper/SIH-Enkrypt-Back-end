3ea2ed64e0b25b5d2892623f5eb9d7452f4ceb2e
Ijlal Ahmad <ijlalahmad845@gmail.com>
2022-08-11 22:18:38 +0530

    optimized login check and validation for multiple sessions

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,58 +1,69 @@
 const User = require("../models/user");
-const bcrypt = require("bcrypt");
 
 const { randomBinary } = require("../utils/utils");
 const generateImagesPattern = require("../utils/generatePattern");
 const { CATS_COUNT, DOGS_COUNT } = require("../config/Constants");
 
 /** =========================== FUNCTION FOR CREATING AND STORING LOGIN PATTERN  ==============================*/
-const createLoginPattern = async (init, user) => {
-  let pattern = user.pattern;
+const createLoginPattern = async (init, user, loginId) => {
+  const session = user.sessions.find((session) => session.loginId === loginId);
+  let pattern;
 
-  //randomBinary function is used to generate pattern
+  //session doesn't exist, creating new session
+  if (!session) {
+    pattern = randomBinary("");
 
-  /*
-  IT WILL RETURN PATTERN WITH LENGTH OF GRIDS_COUNT WHEN PATTERN IS EMPTY
-  IT WILL RETURN PATTERN WITH LENGTH INCREASED BY 1 WHEN PATTERN IS NOT EMPTY
-  */
+    user.sessions.push({
+      loginId,
+      pattern,
+      patternTime: new Date().getTime() + 60 * 1000, //1 minute,
+    });
+  }
+  //session exists, updating pattern and timestamp
+  else {
+    const index = user.sessions.indexOf(session);
+    pattern = randomBinary(session.pattern);
+    user.sessions[index].pattern = pattern;
+  }
 
-  pattern = randomBinary(pattern);
+  await user.save();
+  return pattern;
+};
 
-  // pattern = await bcrypt.hash(pattern, 10);
-  //writing generated pattern to user's data along with timestamp
-  user.pattern = pattern;
-  user.timestamp = new Date().getTime() + 60 * 1000;
-
+/** =========================== FUNCTION FOR CLEARING SESSION FROM DATABASE ==============================*/
+const clearSession = async (user, session) => {
+  const index = user.sessions.indexOf(session);
+  user.sessions.splice(index, 1);
   await user.save();
 };
 
 /** =========================== FUNCTION FOR CLEARING PATTERN FROM DATABASE ==============================*/
-const clearPattern = async (user) => {
-  user.pattern = "";
-  user.timestamp = 0;
+const clearPattern = async (user, session) => {
+  const index = user.sessions.indexOf(session);
+  user.sessions[index].pattern = "";
+  user.sessions[index].patternTime = 0;
   await user.save();
 };
 
 /** =========================== FUNCTION FOR SENDING RANDOM IMAGE PATTERN TO CLIENT ==============================*/
 const imagePattern = async (req, res) => {
-  const { username } = req.body;
+  const { username, loginId } = req.body;
 
-  if (!username) {
+  if (!username || !loginId) {
     return res
       .status(400)
-      .json({ message: "username not found", success: false });
+      .json({ message: "username, loginId not found", success: false });
   }
 
   const user = await User.findOne({ username }).exec();
 
-  //firstly creating login pattern to be stored in DB
-  await createLoginPattern(true, user);
+  //firstly creating login pattern to be stored in DB and also get it
+  const pattern = await createLoginPattern(true, user, loginId);
 
-  let { pattern, category, pass_image } = user;
+  let { category, pass_image } = user;
 
   //getting pass_image value and login pattern value
   pass_image = pass_image.substring(pass_image.length - 1);
-  pattern = pattern.split(":")[0];
 
   let categorySize;
 
@@ -84,33 +95,47 @@ const imagePattern = async (req, res) => {
 
 /** =========================== FUNCTION FOR FINAL VALIDATION OF USER BY PATTERN  ==============================*/
 const validateLogin = async (req, res) => {
-  const { username, pattern, timestamp } = req.body;
+  const { username, loginId, pattern, timestamp } = req.body;
 
-  if (!username || !pattern || !timestamp) {
+  if (!username || !loginId || !pattern || !timestamp) {
     return res.status(400).json({ message: "Empty Field(s)", success: false });
   }
 
   try {
     const user = await User.findOne({ username }).exec();
-    const dbPattern = user.pattern;
-    const dbTimestamp = user.timestamp;
 
-    //checking validity of pattern by timestamp
-    if (timestamp > dbTimestamp) {
-      //clearing pattern from user's data
-        //once pattern is cleared, pattern always show pattern expired, unless user relogin with username
-      await clearPattern(user);
+    //getting session from user loginId
+    const session = user.sessions.find(
+      (session) => session.loginId === loginId
+    );
 
-      return res.status(401).json({
-        message: "Pattern expired",
+    if (!session) {
+      return res.status(409).json({
+        message: "session doesn't exist",
         success: false,
       });
     }
 
-    //comparing pattern
-    if (dbPattern === pattern) {
+    //getting pattern,patternTime from session
+    const dbPattern = session.pattern;
+    const dbTimestamp = session.patternTime;
+
+    //checking validity of pattern by timestamp
+    if (timestamp > dbTimestamp) {
       //clearing pattern from user's data
-      await clearPattern(user);
+      //once pattern is cleared, pattern always show pattern expired, unless user relogin with username
+      await clearPattern(user,session);
+
+      return res.status(401).json({
+        message: "Pattern expired, please relogin",
+        success: false,
+      });
+    }
+
+    //checking pattern
+    if (dbPattern === pattern) {
+      //clearing session after successful login
+      await clearSession(user);
 
       //returning success message
       return res.status(200).json({
@@ -119,9 +144,9 @@ const validateLogin = async (req, res) => {
       });
     } else {
       //for every wrong attemp, pattern is reset and increased by 1
-      await createLoginPattern(false, user);
+      await createLoginPattern(false, user, loginId);
 
-      //returning failure message
+      //unauthorized
       return res.status(401).json({
         message: "pattern not validated",
         success: false,
