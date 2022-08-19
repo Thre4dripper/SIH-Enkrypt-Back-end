e14c4a98a4c45f7ba21930411ee6e8297c68b7e8
Ijlal Ahmad <ijlalahmad845@gmail.com>
2022-08-19 15:53:59 +0530

    implemented warning mail when user rate limit exceeded

--- src/middlewares/userRateLimiter.js
+++ src/middlewares/userRateLimiter.js
@@ -1,38 +1,74 @@
+require('dotenv').config();
+const nodemailer = require("nodemailer");
+const fsPromises = require("fs").promises;
+const path = require("path");
+const moment = require("moment");
+
 const { USER_RATE_LIMIT_MAX } = require("../config/Constants");
 const User = require("../models/user");
+const sendMailPromise = require("../utils/sendMailPromise");
 
 const userRateLimiter = async (req, res, next) => {
-  const { username } = req.body;
+    const { username } = req.body;
 
-  if (!username) {
-    //bad request
-    return res
-      .status(400)
-      .json({ message: "username not found", success: false });
-  }
+    if (!username) {
+        //bad request
+        return res
+            .status(400)
+            .json({ message: "username not found", success: false });
+    }
 
-  const user = await User.findOne({ username });
-  const attempts = user.__v;
-  const lastLogin = user.lastLogin;
-  const timestamp = Date.now();
+    const user = await User.findOne({ username });
+    const attempts = user.__v;
+    const lastLogin = user.lastLogin;
+    const timestamp = Date.now();
 
-  if (attempts >= USER_RATE_LIMIT_MAX && timestamp < lastLogin) {
-    return res
-      .status(401)
-      .json({ message: "too many attempts", success: false });
-  }
+    if (attempts >= USER_RATE_LIMIT_MAX && timestamp < lastLogin) {
+        await sendWarningMail(timestamp, user.email);
+        return res
+            .status(401)
+            .json({ message: "too many attempts", success: false });
+    }
 
-  //reset attempts and lastLogin
-  if (attempts >= USER_RATE_LIMIT_MAX && timestamp > lastLogin) {
-    user.__v = 0;
+    //reset attempts and lastLogin
+    if (attempts >= USER_RATE_LIMIT_MAX && timestamp > lastLogin) {
+        user.__v = 0;
 
-    //it will be filled in createLoginPattern method
-    user.lastLogin = 0;
-  }
+        //it will be filled in createLoginPattern method
+        user.lastLogin = 0;
+    }
 
-  await user.save();
+    await user.save();
 
-  next();
+    next();
 };
+
+/**===================================== FUNCTION FOR SENDING WARNING MAIL ======================================**/
+const sendWarningMail = async (timestamp, email) => {
+    //getting html template
+    const html = await fsPromises.readFile(path.join(__dirname, "..", "views", "warningMail.html"), "utf8");
+    //creating transporter
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.NODEMAILER_USER,
+            pass: process.env.NODEMAILER_PASS
+        }
+    });
+
+    moment.locale("en");
+    const date = moment(timestamp).format("LLLL");
+
+    //creating mail options
+    const mailOptions = {
+        from: process.env.NODEMAILER_USER,
+        to: email,
+        subject: "Warning",
+        html: html.replace("${lastLoginAttempt}", date.toString())
+    }
+    console.log(mailOptions);
+    //sending mail
+    await sendMailPromise(transporter, mailOptions);
+}
 
 module.exports = userRateLimiter;
