c78d6808a1783514708ae936e9c3aae5d0038d45
Ijlal Ahmad <ijlalahmad845@gmail.com>
2022-08-19 12:57:45 +0530

    separated out html templates when sending mails

--- src/controllers/otp/generateOtp.js
+++ src/controllers/otp/generateOtp.js
@@ -1,100 +1,89 @@
 require("dotenv").config();
 const nodemailer = require("nodemailer");
 const bcrypt = require("bcrypt");
+const fsPromises = require("fs").promises;
+const path = require("path");
 const { OTP_EXPIRY_TIME } = require("../../config/Constants");
 const User = require("../../models/user");
 const sendMailPromise = require("../../utils/sendMailPromise");
 
 /**====================== FUNCTION FOR GENERATING,SENDING AND SAVING OTP ========================**/
 const generateOtp = async (req, res) => {
-  const { username } = req.body;
+    const { username } = req.body;
 
-  if (!username) {
-    //bad request
-    return res.status(400).send({
-      message: "Username is required",
-    });
-  }
+    if (!username) {
+        //bad request
+        return res.status(400).send({
+            message: "Username is required",
+        });
+    }
 
-  const user = await User.findOne({ username });
+    const user = await User.findOne({ username });
 
-  //generating 6 digit otp
-  const otp = Math.floor(Math.random() * 1000000);
-  const newOtp = otp.toString().padEnd(6, "0");
+    //generating 6 digit otp
+    const otp = Math.floor(Math.random() * 1000000);
+    const newOtp = otp.toString().padEnd(6, "0");
 
-  try {
-    //sending otp to user's email
-    await clientSendOTP(user, newOtp);
+    try {
+        //sending otp to user's email
+        await clientSendOTP(user, newOtp);
 
-    //saving otp in database
-    await dbSaveOTP(user, newOtp);
+        //saving otp in database
+        await dbSaveOTP(user, newOtp);
 
-    const maskedEmail =
-      user.email.substring(0, 3) +
-      "****" +
-      user.email.substring(user.email.indexOf("@") - 1);
+        const maskedEmail =
+            user.email.substring(0, 3) +
+            "****" +
+            user.email.substring(user.email.indexOf("@") - 1);
 
-    //success
-    res.status(200).json({
-      message: `OTP sent successfully to ${user.username}'s registered email`,
-      email: maskedEmail,
-      success: true,
-    });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).send({
-      message: "Something went wrong",
-    });
-  }
+        //success
+        res.status(200).json({
+            message: `OTP sent successfully to ${user.username}'s registered email`,
+            email: maskedEmail,
+            success: true,
+        });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send({
+            message: "Something went wrong",
+        });
+    }
 };
 
 /**=========================== FUNCTION FOR SAVING OTP IN DATABASE ==================================**/
 const dbSaveOTP = async (user, otp) => {
-  user.otp = await bcrypt.hash(otp.toString(), 10);
-  user.otpTime = Date.now() + OTP_EXPIRY_TIME;
-  await user.save();
+    user.otp = await bcrypt.hash(otp.toString(), 10);
+    user.otpTime = Date.now() + OTP_EXPIRY_TIME;
+    await user.save();
 };
 
 /**=========================== FUNCTION FOR SENDING OTP TO CLIENT ==================================**/
 const clientSendOTP = async (user, otp) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.NODEMAILER_USER,
-      pass: process.env.NODEMAILER_PASS,
-    },
-    host: "smtp.gmail.com",
-    port: 465,
-  });
+    //getting html template
+    const html = await fsPromises.readFile(path.join(__dirname, "..", "..", "views", "otpMail.html"), "utf8");
+    //creating transporter
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.NODEMAILER_USER,
+            pass: process.env.NODEMAILER_PASS,
+        },
+        host: "smtp.gmail.com",
+        port: 465,
+    });
 
-  const brand = "Enkrypt";
+    const brand = "Enkrypt";
 
-  const mailOptions = {
-    from: process.env.NODEMAILER_USER,
-    to: user.email,
-    subject: "Enkrypt Password Reset",
-    html: `
-<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
-      <div style="margin:50px auto;width:70%;padding:20px 0">
-        <div style="border-bottom:1px solid #eee">
-          <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">${brand}</a>
-        </div>
-        <p style="font-size:1.1em">Hi,</p>
-        <p>Thank you for choosing ${brand}. Use the following OTP to reset your pass image. OTP is valid for 5 minutes</p>
-        <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${otp}</h2>
-        <p style="font-size:0.9em;">Regards,<br />${brand}</p>
-        <hr style="border:none;border-top:1px solid #eee" />
-        <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
-          <p>${brand} Inc</p>
-          <p>Jamia Millia Islamia</p>
-          <p>New Delhi</p>
-        </div>
-      </div>
-    </div>
-`,
-  };
+    //creating mail options
+    const mailOptions = {
+        from: process.env.NODEMAILER_USER,
+        to: user.email,
+        subject: "Enkrypt Password Reset",
+        html: html.replace("${otp}", otp).replace("${brand}", brand),
+    };
 
-  return await sendMailPromise(transporter, mailOptions);
+    //sending mail
+    return await sendMailPromise(transporter, mailOptions);
 };
 
 
--- src/controllers/signup/sendConfirmMail.js
+++ src/controllers/signup/sendConfirmMail.js
@@ -1,7 +1,14 @@
 require('dotenv').config();
 const nodemailer = require("nodemailer");
+const fsPromises = require("fs").promises;
+const path = require("path");
 const sendMailPromise = require("../../utils/sendMailPromise");
-const sendConfirmMail = async (username,email) => {
+
+/**============================ FUNCTION FOR SENDING CONFIRMATION MAIL OF ID CREATED ==================================**/
+const sendConfirmMail = async (username, email) => {
+    //getting html template
+    const html = await fsPromises.readFile(path.join(__dirname, "..", "..", "views", "confirmationMail.html"), "utf8");
+    //creating transporter
     const transporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
@@ -9,107 +16,15 @@ const sendConfirmMail = async (username,email) => {
             pass: process.env.NODEMAILER_PASS
         }
     });
+    //creating mail options
     const mailOptions = {
         from: process.env.NODEMAILER_USER,
         to: email,
         subject: "ID Created Successfully",
-        html: `
-        <div style="margin: 0; padding: 0; font-family:'sans-serif';color:#6e6e6e;">
- <table border="0" cellpadding="0" cellspacing="0" width="100%">
-  <tr>
-   <td>
-      <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="border-collapse: collapse;">
-     <tr>
-      <td align="left" bgcolor="#fb6519" style="padding: 30px 0 30px 30px;color: #ffffff;font-weight: 700;font-size:24px;font-family: 'sans-serif';letter-spacing: 7px;">
-       CONFIRMATION MAIL
-      </td>
-     </tr>
-     <tr>
-      <td bgcolor="#f2f2f2" style="padding: 40px 30px 40px 30px;">
-        <!-- content area -->
-          <table border="0" cellpadding="0" cellspacing="0" width="100%">
-             <tr>
-               <td align="center" style="padding-bottom:20px;color: #04B45F;font-size:24px;font-weight:bolder;">
-                Account Confirmed !
-               </td>
-              </tr>
-            <tr>
-   <td align="center">
-    <img src="https://firebasestorage.googleapis.com/v0/b/enkrypt-4fbe3.appspot.com/o/WhatsApp%20Image%202022-08-06%20at%2011.43.46%20PM.jpeg?alt=media&token=f6f42729-3da4-448c-bcb9-3bcb0bbb3f2c">
-   </td>
-  </tr>
-  <tr>
-   <td style="padding-top:50px;">
-    Hi,
-   </td>
-  </tr>
-  <tr>
-   <td style="padding-top:20px;">
-    Welcome !
-   </td>
-  </tr>
-  <tr>
-   <td style="padding-top:20px;">
-    Thank you for joining us ${username}.Your account is confirmed. You now have access to login.
-   </td>
-  </tr>
-  <tr>
-   <td style="padding-top:10px;">
-     You may now sign in,
-   </td>
-  </tr>
-   <tr>
-   <td align="center" style="padding-top:20px;">
-     <a href="#" style="padding: 10px 25px;background-color: #fb6519;color: #ffffff; border:none;border-radius:4px;display:block;width:200px;text-align:center;text-decoration:none;">Sign in</a>
-   </td>
-  </tr>     
-   <tr>
-   <td style="padding-top:20px;">
-    If you have any questions or concerns please reach out to your dedicated Universum contact, or email us any time at <a href="#" style="color:#fb6519;">sih.enkrypt@gmail.com</a> if you have technical issues.
-   </td>
-  </tr>
-  <tr>
-   <td style="padding-top:20px;">
-    Best Regards,
-   </td>
-  </tr>
-  <tr>
-   <td>
-    Team Enkrypt
-   </td>
-  </tr>
- </table>
-        <!-- content area ends  -->
-      </td>
-     </tr>
-        <tr>
-      <td bgcolor="#2A2F43" style="padding: 20px 10px ">
-       <!-- footer area -->
-        <table border="0" cellpadding="0" cellspacing="0" width="100%">
-       <tr>
-        <td style="color:#ffffff;font-size:12px;">
-         &copy; 2022 All Rights Reserved.
-        </td>
-        <td align="right">
-          <table border="0" cellpadding="0" cellspacing="0">
-  <tr>
-   <td style="font-size: 0; line-height: 0;" width="20">&nbsp;</td>
-  </tr>
- </table>
-        </td>
-       </tr>
-      </table>
-       <!-- footer ends -->
-      </td>
-     </tr>
-    </table>
-   </td>
-  </tr>
- </table>
-</body>
-        `
+        html:html.replace("${username}", username)
     }
 
+    //sending mail
     await sendMailPromise(transporter, mailOptions);
 }
 
